795e68143409a17360652ea9c7c4f17bde4a6e95
Deanang <ngeekeng@gmail.com>
2017-07-12 02:30:12 +0800

    1
    1

---
+++ memo.py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(20000)
+from time import time
+
+def fib(x):
+  if x == 1 or x == 2:
+    return 1
+  else:
+    return fib(x-1) + fib(x-2)
+
+start = time()
+fib(30)
+end = time()
+print(end-start)
+
+def memo(f):
+    d = dict()
+    def helper(x):
+        if x not in d:
+            d[x] = f(x)
+        return d[x]
+    return helper
+
+fib = memo(fib)
+
+start = time()
+fib(5000)
+end = time()
+print(end-start)
